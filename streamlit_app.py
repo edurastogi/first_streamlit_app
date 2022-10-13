bac1e501350924e56e07c18685065e55687bcd07
Aditya Rastogi <arastogi2002@gmail.com>
2022-10-12 22:27:42 -0400

    Update streamlit_app.py
    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit as sl
 
 sl.title('My Parents New Healthy Diner')
 
+sl.header('Breakfast Menu')
+sl.text('Omega 3 & Blueberry Oatmeal')
+sl.text('Kale, Spinach & Rocket Smoothie')
+sl.text('Hard-Bolied Free-Range Egg')
 
