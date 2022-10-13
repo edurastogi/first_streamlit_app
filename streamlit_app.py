2df86a1d06d3c17b5b298471923f2635232a0efd
Aditya Rastogi <arastogi2002@gmail.com>
2022-10-12 22:31:16 -0400

    Update streamlit_app.py
    Added more details

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import streamlit as sl
 sl.title('My Parents New Healthy Diner')
 
 sl.header('Breakfast Menu')
-sl.text('Omega 3 & Blueberry Oatmeal')
-sl.text('Kale, Spinach & Rocket Smoothie')
-sl.text('Hard-Bolied Free-Range Egg')
+sl.text('🥣 Omega 3 & Blueberry Oatmeal')
+sl.text('🥗 Kale, Spinach & Rocket Smoothie')
+sl.text('🐔 Hard-Bolied Free-Range Egg')
+sl.text('🥑🍞 Avocado Toast')
+
+sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
